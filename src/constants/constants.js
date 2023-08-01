091d3092e2f622a9b8bec1cae249cf19b81c4fe0
vecoweb <vecoweb22@yandex.ru>
2023-08-01 06:58:06 +0300

    Изменение в constants и удаление MaketDiploma

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,7 @@
-const { APP_BASE_URL, NODE_ENV } = process.env;
-export const BASE_URL = NODE_ENV === 'development' ? 'http://localhost:3000' : APP_BASE_URL;
+const { NODE_ENV } = process.env;
+// const { APP_BASE_URL, NODE_ENV } = process.env;
+export const BASE_URL = NODE_ENV === 'prodaction' === 'https://api.vecowebmovies.nomoreparties.sbs';
+// export const BASE_URL = NODE_ENV === 'development' ? 'http://localhost:3000' : APP_BASE_URL;
 export const BEATFILM_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 export const MOVIES_IMAGES_URL = 'https://api.nomoreparties.co';
 export const SCREEN_TABLET = 768;
